90b869506c662c6903fb72f7541d0cecf1e12e03
yadav <s6anyada@uni-bonn.de>
2020-03-09 16:49:54 +0100

    Added A plot that is closest to a line plot. Output as in ref2.pdf

--- ROOT/Traffic2.C
+++ ROOT/Traffic2.C
@@ -6,6 +6,50 @@
 using namespace std;
 
 //Function for plotting
+void ReverseYAxis (TMultiGraph *h);
+void ReverseYAxis (TMultiGraph *h)
+{
+   // Remove the current axis
+   h->GetYaxis()->SetLabelOffset(999);
+   h->GetYaxis()->SetTickLength(0);
+
+   // Redraw the new axis
+   gPad->Update();
+   TGaxis *newaxis = new TGaxis(gPad->GetUxmin(),
+                                gPad->GetUymax(),
+                                gPad->GetUxmin()-0.001,
+                                gPad->GetUymin(),
+                                h->GetYaxis()->GetXmin()-1,
+                                h->GetYaxis()->GetXmax()+2,
+                                510,"+");
+   newaxis->SetLabelOffset(-0.03);
+   newaxis->SetTitle(" Time ---->>>");
+   newaxis->SetLabelSize(0.035);
+   newaxis->Draw();
+}
+
+
+void ShiftXAxis (TMultiGraph *h, int Xmax);
+void ShiftXAxis (TMultiGraph *h, int Xmax)
+{
+   // Remove the current axis
+   h->GetXaxis()->SetLabelOffset(999);
+   h->GetXaxis()->SetTickLength(0);
+
+   // Redraw the new axis
+   gPad->Update();
+   TGaxis *newaxis = new TGaxis(gPad->GetUxmin(),
+                                gPad->GetUymax(),
+                                gPad->GetUxmax(),
+                                gPad->GetUymax(),
+                                -1,
+                                Xmax+2,
+                                510,"-");
+   newaxis->SetLabelOffset(0.);
+   newaxis->SetLabelSize(0.035);
+   newaxis->SetTitle(" Position ---->>>");
+   newaxis->Draw();
+}
 
 
 //Class for keeping info o One car
@@ -71,24 +115,7 @@ class Vehical: public TimeStamp
 	{
 		cout << " \nTime\tPosition\tvelocity\n " << endl;
 		cout << CarData[i].Time <<"\t"<< CarData[i].Position <<"\t"<< CarData[i].Velocity << endl;
-	}    
-    
-// Plotting Function    
-    void PlotIt( Vehical a, int id, int Tmax,TGraph *step )
-    {
-
-        for (int j=0; j<=Tmax; j++)
-        {   
-            step->SetPoint(j,a.CarData[j].Position,a.CarData[j].Time);
-            step->Draw("same A*");
-            step->SetMarkerStyle(id+1);
-
-        }   
-    
-    
-        //delete step;
-    } 
-    
+	}        
 };
 
 //Probability Function
@@ -106,31 +133,30 @@ int Traffic2()
 	
 	srand(time(NULL));
 	cout<<"This should work!!!\n\n\n";
-	int TotCars = 5;
-	int TotSites = 20;
+	int TotCars = 9;
+	int TotSites = 70;
 	int Tmax = 30;
 	int Vmax = 5;
 	float p = 0.5;
 	int x2=0,x=0,v=0,d=0;
 	Vehical car[TotCars];
 //Initialisation Part
-//	int xfirst
-/*
-	cars[0].SetCarData(0,0,rand()%Vmax,rand()%Vmax);
-	for (int i = 1; i < TotCars; i++)
-	{
-		v = rand()%Vmax;
-		x = car[i-1].GetCarPosition(0) + rand()%car[]
-		car[i].SetVehicalID(i+1);
-//		cars[i].SetCarData(0,0)
-	}	
-*/	
 	
-	car[0].SetCarData(0,0,3,5);
-	car[1].SetCarData(0,0,8,4);	
-	car[2].SetCarData(0,0,14,0);
-	car[3].SetCarData(0,0,15,0);
-	car[4].SetCarData(0,0,18,3);	
+	car[0].SetCarData(0,0,9,4);
+	car[1].SetCarData(0,0,26,5);	
+	car[2].SetCarData(0,0,33,5);
+	car[3].SetCarData(0,0,40,5);
+	car[4].SetCarData(0,0,49,0);	
+	car[5].SetCarData(0,0,50,0);
+	car[6].SetCarData(0,0,52,1);
+	car[7].SetCarData(0,0,60,4);
+	car[8].SetCarData(0,0,70,5);
+	
+//	car[0].SetCarData(0,0,3,5);
+//	car[1].SetCarData(0,0,8,4);	
+//	car[2].SetCarData(0,0,14,0);
+//	car[3].SetCarData(0,0,15,0);
+//	car[4].SetCarData(0,0,18,3);
 			
 
 	for (int i = 1; i <= Tmax; i++)
@@ -140,7 +166,7 @@ int Traffic2()
 			x = car[j].CarData[i-1].Position;
 			v = car[j].CarData[i-1].Velocity;
 			
-			if (j+1 == 5)
+			if (j+1 == TotCars)
 				x2 = car[0].CarData[i-1].Position;
 			else
 				x2 = car[j+1].CarData[i-1].Position;
@@ -153,7 +179,7 @@ int Traffic2()
 			if (v < Vmax && d > v + 1 )
 					v = v + 1; 
 //STEP 2		
-			if (d <= v && v > 0)
+			if (d <= v && v > 0 )
 				v = d - 1;
 //STEP 3
 			if (Probability(p) == true && v > 0)
@@ -186,13 +212,13 @@ int flag =0;
 				if (car[k].CarData[i].Position == j)
 				{
 					flag=1;
-					cout << car[k].CarData[i].Velocity << "\t";
+					cout << car[k].CarData[i].Velocity << "";
 					break;
 				}
 			}//for comparing every car's position
 			
 			if (flag == 0)
-				cout << ".\t" ;
+				cout << "." ;
 			
 		}// for every site
 		
@@ -200,23 +226,32 @@ int flag =0;
 	}//For all time values
 	
 	
-	
-	
+		
 	
 //Plotting
 	TCanvas *c1 = new TCanvas("c1","Graph Draw Options",200,10,800,800);
-    c1->SetGrid();
-	TGraph *step = new TGraph();
+//    c1->SetGrid();
+    TGraph *step[TotCars];
+    TMultiGraph *mg = new TMultiGraph();
 	for (int i=0; i<TotCars; i++)
 	{
-	    car[i].PlotIt(car[i],i,Tmax,step);
-	    //c1->Update();
+	    step[i] = new TGraph();
+	    for (int j=0; j<=Tmax; j++)
+        {   
+            step[i]->SetPoint(j,car[i].CarData[j].Position,Tmax-car[i].CarData[j].Time);
+            //step[i]->Draw("same A*");
+            step[i]->SetMarkerStyle(i*2+1);
+            c1->Update();
+
+        } 
+        mg->Add(step[i]); 
+        step[i]->SetMarkerStyle(20+i);
+        step[i]->SetMarkerColor(i+1);        
 	}
-	
-	
+	mg->Draw("A pm p");
+	ShiftXAxis(mg,TotSites);
+	ReverseYAxis(mg);
 	return 0;
 }
-
-
 
 
